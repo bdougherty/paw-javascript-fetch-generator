b6f9b968077b9df70b0ac949d4e61583f4ae724c
Brad Dougherty <me@brad.is>
2017-12-29 17:22:50 -0500

    Initial commit

---
+++ JavaScriptFetchGenerator.js
@@ -0,0 +1,77 @@
+class JavaScriptFetchGenerator {
+	static get identifier() {
+		return 'com.secretspaceagency.JavaScriptFetchGenerator';
+	}
+
+	static get title() {
+		return 'JavaScript (Fetch)';
+	}
+
+	static get fileExtension() {
+		return 'js';
+	}
+
+	static get languageHighlighter() {
+		return 'javascript';
+	}
+
+	getResponseTypeFromContentType(contentType = '') {
+		if (contentType.indexOf('json') !== -1) {
+			return 'json';
+		}
+
+		return 'text';
+	}
+
+	generate(context, requests) {
+		if (!Array.isArray(requests)) {
+			requests = [requests];
+		}
+
+		let code = '';
+
+		for (const request of requests) {
+			const latestExchange = request.getLastExchange();
+			const contentType = latestExchange ? latestExchange.getResponseHeaderByName('Content-Type') : '';
+			const responseFormat = this.getResponseTypeFromContentType(contentType);
+
+			const fetchOptions = [];
+
+			if (request.method !== 'GET') {
+				fetchOptions.push(`method: '${request.method}'`);
+			}
+
+			if (Object.keys(request.headers).length > 0) {
+				const headerString = JSON.stringify(request.headers, null, '\t\t\t').replace(/}$/, '\t\t}');
+				fetchOptions.push(`headers: ${headerString}`);
+			}
+
+			if (request.body) {
+				if (request.method === 'GET' || request.method === 'HEAD') {
+					fetchOptions.push('// fetch does not support a body with GET or HEAD requests');
+				}
+				else {
+					fetchOptions.push(`body: \`${request.body}\``);
+				}
+			}
+
+			if (!request.followRedirects) {
+				fetchOptions.push(`redirect: 'error'`);
+			}
+
+			const optionsString = fetchOptions.length < 1 ? '' : `, {
+		${fetchOptions.join(',\n\t\t')}
+	}`;
+
+			code += `// ${request.name}
+async function makeRequest() {
+	const response = await fetch('${request.url}'${optionsString});
+	return response.${responseFormat}();
+}`;
+		}
+
+		return code;
+	}
+}
+
+registerCodeGenerator(JavaScriptFetchGenerator);
---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript Fetch Code Generator (Paw Extension)
+
+A [Paw Extension](https://paw.cloud/extensions/) that generates JavaScript code for requests using [the fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API).
+
+## License
+
+MIT © [Brad Dougherty](https://brad.is)
